f7ead67438afadc8c031c51d281b8d6a2388d07f
Abolfazl Saffari <abolfazl_drx@yahoo.com>
2023-04-13 02:15:41 +0430

    feat: payment result page added

---
+++ src/Components/Payment/ResultDescription.jsx
@@ -0,0 +1,7 @@
+const ResultDescription = ({ children }) => {
+  return (
+    <p className="w-64 text-xl md:w-96 text-justify md:text-2xl">{children}</p>
+  );
+};
+
+export default ResultDescription;
