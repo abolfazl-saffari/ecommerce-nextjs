dd7d5ad97f8617176b91f353fdd10c8ca40e58d7
Abolfazl Saffari <abolfazl_drx@yahoo.com>
2023-04-13 15:31:02 +0430

    feat: login form added

--- src/Components/Panels/Admin/LoginForm.jsx
+++ src/Components/Panels/Admin/LoginForm.jsx
@@ -1,43 +1,34 @@
+import FormInput from "./FormInput";
+import Button from "@/Components/UI/Button";
+import BackToHome from "./BackToHome";
+
 const LoginForm = () => {
   return (
-    <div className="h-screen flex justify-center items-center bg-gray-400">
-      <div class="w-full relative max-w-md bg-slate-200 py-14 px-8 rounded-md">
-        <h2 className="text-center text-3xl">ورود به پنل مدیریت فروشگاه</h2>
-        <form className="mt-8" action="#" method="POST">
-          <div className="rounded-md shadow-sm">
-            <div className="mb-3">
-              <label htmlFor="email-address">نام کاربری:</label>
-              <input
-                id="email-address"
-                name="email"
-                type="email"
-                autocomplete="email"
-                required
-                className="relative block w-full py-1.5 mt-2"
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="password">رمزعبور:</label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autocomplete="current-password"
-                required
-                className="relative block w-full py-1.5 mt-2"
-              />
-            </div>
-          </div>
-          <button
-            type="submit"
-            className="w-full mt-5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
+    <div className="h-screen flex justify-center items-center bg-gray-500">
+      <div class="w-full relative max-w-md bg-slate-900 py-14 px-8 rounded-md">
+        <h2 className="text-center text-3xl text-white">
+          ورود به پنل مدیریت فروشگاه
+        </h2>
+        <form className="mt-8">
+          <FormInput
+            id="email-address"
+            name="email"
+            type="email"
+            autocomplete="email"
+            label="نام کاربری:"
+          />
+          <FormInput
+            id="password"
+            name="password"
+            type="password"
+            autocomplete="current-password"
+            label="رمزعبور:"
+          />
+          <Button className="w-full text-sm px-3 py-2 mt-5 rounded-md bg-slate-700">
             ورود
-          </button>
+          </Button>
         </form>
-        <a className="text-sm text-blue-500 absolute left-2.5 bottom-2.5 hover:underline">
-          بازگشت به سایت
-        </a>
+        <BackToHome className="absolute left-2.5 bottom-2.5" />
       </div>
     </div>
   );
