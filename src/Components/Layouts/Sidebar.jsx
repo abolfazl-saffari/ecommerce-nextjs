dbc0eb974da020433bdf18f3e29499e44348ae3e
Abolfazl Saffari <abolfazl_drx@yahoo.com>
2023-04-15 06:15:38 +0430

    feat: side bar added; side bar layout added; product wrapper now is reusable  for both side bar layout and simple layout

---
+++ src/Components/Layouts/Sidebar.jsx
@@ -0,0 +1,63 @@
+const Sidebar = () => {
+  return (
+    <div className="hidden lg:block col-span-1">
+      {/* <div className="fixed top-[64px]"> */}
+      <div className="mb-5">
+        <p>کالاهای اساسی و خوار و بار</p>
+        <ul>
+          <li className="ms-4">نان</li>
+          <li className="ms-4">برنج</li>
+          <li className="ms-4">روغن</li>
+          <li className="ms-4">قند و نبات</li>
+        </ul>
+      </div>
+      <div className="mb-5">
+        <p>کالاهای اساسی و خوار و بار</p>
+        <ul>
+          <li className="ms-4">نان</li>
+          <li className="ms-4">برنج</li>
+          <li className="ms-4">روغن</li>
+          <li className="ms-4">قند و نبات</li>
+        </ul>
+      </div>
+      <div className="mb-5">
+        <p>کالاهای اساسی و خوار و بار</p>
+        <ul>
+          <li className="ms-4">نان</li>
+          <li className="ms-4">برنج</li>
+          <li className="ms-4">روغن</li>
+          <li className="ms-4">قند و نبات</li>
+        </ul>
+      </div>
+      <div className="mb-5">
+        <p>کالاهای اساسی و خوار و بار</p>
+        <ul>
+          <li className="ms-4">نان</li>
+          <li className="ms-4">برنج</li>
+          <li className="ms-4">روغن</li>
+          <li className="ms-4">قند و نبات</li>
+        </ul>
+      </div>
+      <div className="mb-5">
+        <p>کالاهای اساسی و خوار و بار</p>
+        <ul>
+          <li className="ms-4">نان</li>
+          <li className="ms-4">برنج</li>
+          <li className="ms-4">روغن</li>
+          <li className="ms-4">قند و نبات</li>
+        </ul>
+      </div>
+      <div className="mb-5">
+        <p>کالاهای اساسی و خوار و بار</p>
+        <ul>
+          <li className="ms-4">نان</li>
+          <li className="ms-4">برنج</li>
+          <li className="ms-4">روغن</li>
+          <li className="ms-4">قند و نبات</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
