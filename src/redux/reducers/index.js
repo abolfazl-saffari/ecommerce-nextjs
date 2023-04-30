6a043a2b7f3ea36e9c69ee62b97a7693d3290185
Abolfazl Saffari <abolfazl_drx@yahoo.com>
2023-04-30 20:48:14 +0430

    feat: categories redux added

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import productsReducer from "./productsReducer";
 import productReducer from "./productReducer";
+import categoriesReducer from "./categoriesReducer";
 
 export default combineReducers({
   products: productsReducer,
   product: productReducer,
+  categories: categoriesReducer,
 });
