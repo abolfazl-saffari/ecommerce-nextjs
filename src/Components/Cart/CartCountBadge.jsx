d7090041084a9d0740cf7eb2c9ce9b62323f5efa
Abolfazl Saffari <abolfazl_drx@yahoo.com>
2023-05-01 19:35:37 +0430

    feat: cart connected to redux

--- src/Components/Cart/CartCountBadge.jsx
+++ src/Components/Cart/CartCountBadge.jsx
@@ -1,4 +1,8 @@
-const CartCountBadge = ({ count = 0 }) => {
+import { useSelector } from "react-redux";
+
+const CartCountBadge = () => {
+  const count = useSelector((store) => store).cart.totalItems;
+
   return (
     <span className="w-5 h-5 absolute flex items-center justify-center text-xs -right-3 -bottom-2 rounded-md text-white bg-red-800">
       {count}
