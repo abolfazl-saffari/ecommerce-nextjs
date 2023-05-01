1ff2552cf6e3485b41e9814261bdf03ca6a1f8ca
Abolfazl Saffari <abolfazl_drx@yahoo.com>
2023-05-01 21:03:37 +0430

    feat: Button type atr added; ProductAction connected to redux; SingleProduct pass product obj to ProductAction

--- src/Components/Products/ProductActions.jsx
+++ src/Components/Products/ProductActions.jsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addToCart } from "@/redux/actions/cartAction";
 import Button from "../UI/Button";
 
-const ProductActions = () => {
+const ProductActions = ({ product }) => {
+  const [amount, setAmount] = useState(1);
+  const dispatch = useDispatch();
+
+  const amountHandler = (e) => {
+    setAmount(e.target.value);
+  };
+  const totalSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const changeInv = { ...product, inventory: amount };
+    dispatch(addToCart(changeInv));
+  };
+
   return (
-    <div className="flex items-center gap-5">
+    <form onSubmit={totalSubmitHandler} className="flex items-center gap-5">
       <input
+        value={amount}
+        onChange={amountHandler}
         type="number"
         id="small-input"
         className="w-14 rounded-lg border py-3 px-2 bg-gray-50 text-gray-900 border-gray-400 sm:text-xs"
-        defaultValue="1"
         min="1"
-        max="10"
+        max={product.inventory}
       />
-      <Button className="px-4 bg-green-600">افزودن به سبد خرید</Button>
-    </div>
+      <Button type={"submit"} className="px-4 bg-green-600">
+        افزودن به سبد خرید
+      </Button>
+    </form>
   );
 };
 
--- src/Components/UI/Button.jsx
+++ src/Components/UI/Button.jsx
@@ -1,6 +1,7 @@
-const Button = ({ className, children, onClick, disabled }) => {
+const Button = ({ className, children, onClick, disabled, type }) => {
   return (
     <button
+      type={type ? type : "button"}
       disabled={disabled}
       className={`${className} rounded font-semibold border py-2 text-white border-white hover:text-white hover:bg-slate-800 hover:border-transparent`}
       onClick={onClick}
