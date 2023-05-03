739cb1d5394a7d9b9a2e17e9696f9e7f7148d321
Abolfazl Saffari <abolfazl_drx@yahoo.com>
2023-05-03 18:43:51 +0430

    feat: addToCart action in cart reducer logic changed for fixing a bug adding a same item

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -16,7 +16,25 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART:
-      const updatedStoreAdd = state.cart.concat(action.payload);
+      console.log(state.cart.find((item) => item.id === action.payload.id));
+      console.log(action.payload.id);
+
+      let updatedStoreAdd;
+      if (state.cart.find((item) => item.id === action.payload.id)) {
+        updatedStoreAdd = state.cart.map((item) =>
+          item.id === action.payload.id
+            ? {
+                ...item,
+                inventory: +item.inventory + +action.payload.inventory,
+              }
+            : { ...item }
+        );
+        console.log("tekrari");
+      } else {
+        updatedStoreAdd = state.cart.concat(action.payload);
+        console.log("motefafet");
+      }
+
       const totalAdd = state.totalItems + +action.payload.inventory;
       return {
         ...state,
