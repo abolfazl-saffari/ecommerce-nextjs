ebfe19e9cbc49a923e7f65a11f8f611c216010ff
Abolfazl Saffari <abolfazl_drx@yahoo.com>
2023-05-04 01:53:54 +0430

    feat: db.json orders is empty; decreaseInventoryProduct added to redux

--- src/redux/actions/productsAction.js
+++ src/redux/actions/productsAction.js
@@ -1,4 +1,9 @@
-import { GET_PRODUCTS, PRODUCTS_ERROR } from "../types";
+import {
+  GET_PRODUCTS,
+  PRODUCTS_ERROR,
+  DECREASING_PRODUCT_INVENTORY,
+  ERROR_DECREASING_PRODUCT_INVENTORY,
+} from "../types";
 import axios from "axios";
 
 export const getProducts = () => async (dispatch) => {
@@ -15,3 +20,24 @@ export const getProducts = () => async (dispatch) => {
     });
   }
 };
+
+export const decreasingProductInventory =
+  (productId, remainingItems) => async (dispatch) => {
+    try {
+      const res = await axios.patch(
+        `http://localhost:3004/products/${productId}`,
+        {
+          inventory: remainingItems,
+        }
+      );
+      dispatch({
+        type: DECREASING_PRODUCT_INVENTORY,
+        payload: res.data,
+      });
+    } catch (e) {
+      dispatch({
+        type: ERROR_DECREASING_PRODUCT_INVENTORY,
+        payload: console.log(e),
+      });
+    }
+  };
