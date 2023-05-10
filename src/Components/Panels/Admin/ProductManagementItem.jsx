9784b77b65612c1e1144eaa70262820e4074d400
Abolfazl Saffari <abolfazl_drx@yahoo.com>
2023-05-11 00:44:46 +0330

    feat: productManagement compnent connected to Api and redux

--- src/Components/Panels/Admin/ProductManagementItem.jsx
+++ src/Components/Panels/Admin/ProductManagementItem.jsx
@@ -1,22 +1,55 @@
-const ProductManagementItem = ({ onShowModal }) => {
+import { useDispatch } from "react-redux";
+import { removeProduct } from "@/redux/actions/productsAction";
+import Anchor from "./Anchor";
+
+const ProductManagementItem = ({
+  id,
+  title,
+  category,
+  subCategory,
+  image,
+  onShowModal,
+}) => {
+  const dispatch = useDispatch();
+  const removeProductHandler = () => {
+    dispatch(removeProduct(id));
+  };
+
   return (
     <tr className="border-t-2 text-black bg-gray-300 border-slate-800 hover:bg-gray-50">
       <th scope="row" className="font-medium whitespace-nowrap px-6 py-4 ">
         <a href="#"></a>
       </th>
-      <td className="px-6 py-4"> روغن سرخ کردنی سمن - 1.35 کلیویی</td>
-      <td className="px-6 py-4">مواد غذایی / کالاهای اساسی و خوار و بار</td>
+      <td className="px-6 py-4">
+        <Anchor
+          className="text-gray-900"
+          href={`/${category}/${subCategory}/${id}`}
+        >
+          {title}
+        </Anchor>
+      </td>
+      <td className="px-6 py-4">
+        <Anchor className="text-gray-900" href={`/${category}`}>
+          {category}
+        </Anchor>{" "}
+        /{" "}
+        <Anchor className="text-gray-900" href={`/${category}/${subCategory}`}>
+          {subCategory}
+        </Anchor>
+      </td>
       <td className="px-6  flex gap-5 py-4">
-        <a
-          href="#"
+        <p
           onClick={onShowModal}
-          className="font-medium text-blue-600 hover:underline"
+          className="font-medium text-blue-600 cursor-pointer hover:underline"
         >
           ویرایش
-        </a>
-        <a href="#" className="font-medium text-blue-600 hover:underline">
+        </p>
+        <p
+          onClick={removeProductHandler}
+          className="font-medium text-blue-600 cursor-pointer hover:underline"
+        >
           حذف
-        </a>
+        </p>
       </td>
     </tr>
   );
