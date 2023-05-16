c64cae75376375b1a479ec280ed8afdd7417c4bb
Abolfazl Saffari <abolfazl_drx@yahoo.com>
2023-05-16 17:16:28 +0330

    feat: draft js package removed; ordersMangement connected to redux; orders reducer added to redux; payment order config added radioInputs onchange method added; db json changed for test actions

--- src/Components/Panels/Admin/ProductManagementModal.jsx
+++ src/Components/Panels/Admin/ProductManagementModal.jsx
@@ -1,24 +1,21 @@
 import { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCategories } from "@/redux/actions/categoriesAction";
+import { addProduct } from "@/redux/actions/productsAction";
 import Modal from "@/Components/UI/Modal";
 import Button from "@/Components/UI/Button";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import dynamic from "next/dynamic";
-
-const Weditor = dynamic(
-  () => import("react-draft-wysiwyg").then((mod) => mod.Editor),
-  { ssr: false }
-);
 
 const ProductManagementModal = ({ showModal, onHideModal }) => {
   const { categories } = useSelector((store) => store).categories;
   const [subCategories, setSubCategories] = useState([]);
+  const [newCategoryAdder, setNewCategoryAdder] = useState(false);
+  const [newSubcategoryAdder, setNewSubcategoryAdder] = useState(false);
   const dispatch = useDispatch();
-  const userTitleInputRef = useRef();
-  const userImageInputRef = useRef();
-  const userCategoryInputRef = useRef();
-  const userSubcategoryInputRef = useRef();
+  const userTitleInputRef = useRef(null);
+  const userImageInputRef = useRef(null);
+  const userCategoryInputRef = useRef(null);
+  const userSubcategoryInputRef = useRef(null);
+  const userDescriptionInputRef = useRef(null);
 
   useEffect(() => {
     dispatch(getCategories());
@@ -33,16 +30,43 @@ const ProductManagementModal = ({ showModal, onHideModal }) => {
         ? subCategoriesOfCurrentCategory.subCategories
         : []
     );
+    if (e.target.value === "newCategory") {
+      setNewCategoryAdder(true);
+      setNewSubcategoryAdder(false);
+    } else {
+      setNewCategoryAdder(false);
+      setNewSubcategoryAdder(false);
+    }
+  };
+
+  const userSubcategorySelectedHandler = (e) => {
+    if (e.target.value === "newSubcategory") {
+      setNewSubcategoryAdder(true);
+    } else {
+      setNewSubcategoryAdder(false);
+    }
   };
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
-    console.log(
-      userTitleInputRef.current.value,
-      userImageInputRef.current.files[0],
-      userCategoryInputRef.current.value,
-      userSubcategoryInputRef.current.value
-    );
+
+    const newProduct = {
+      title: userTitleInputRef.current.value,
+      price: 0,
+      inventory: 0,
+      category: userCategoryInputRef.current.value,
+      subCategory: userSubcategoryInputRef.current.value,
+      image: [userImageInputRef.current.files],
+      description: userDescriptionInputRef.current.value,
+    };
+
+    dispatch(addProduct(newProduct));
+    userTitleInputRef.current.value = "";
+    userImageInputRef.current.value = "";
+    userCategoryInputRef.current.value = "";
+    userSubcategoryInputRef.current.value = "";
+    userDescriptionInputRef.current.value = "";
+    onHideModal();
   };
 
   return (
@@ -60,6 +84,7 @@ const ProductManagementModal = ({ showModal, onHideModal }) => {
             تصویر کالا:
           </label>
           <input
+            required
             ref={userImageInputRef}
             className="w-full block ltr text-sm border rounded-lg cursor-pointer mb-6  text-gray-900 border-gray-300 bg-gray-50"
             id="file_input"
@@ -73,6 +98,7 @@ const ProductManagementModal = ({ showModal, onHideModal }) => {
             نام کالا:
           </label>
           <input
+            required
             ref={userTitleInputRef}
             className="w-full block text-sm border rounded-lg p-1 mb-6 text-gray-900 border-gray-300 bg-gray-50"
             id="text_input"
@@ -86,18 +112,28 @@ const ProductManagementModal = ({ showModal, onHideModal }) => {
           </label>
           <select
             ref={userCategoryInputRef}
-            className="w-full block text-sm rounded-lg cursor-pointer p-1 mb-6 text-gray-900 border border-gray-300 bg-gray-50"
+            className={`w-full block text-sm rounded-lg cursor-pointer p-1 ${
+              newCategoryAdder ? "mb-2" : "mb-6"
+            } text-gray-900 border border-gray-300 bg-gray-50`}
             id="category-input"
             onChange={userCategorySelectedHandler}
           >
-            <option>لطفا یک دسته را انتخاب کنید ...</option>
+            <option value="">لطفا یک دسته را انتخاب کنید ...</option>
             {categories.map((category) => (
               <option value={category.mainCategory}>
                 {category.mainCategory}
               </option>
             ))}
-            <option>دسته بندی مورد نظر خود را وارد کنید.</option>
+            <option value="newCategory">
+              دسته بندی مورد نظر خود را وارد کنید.
+            </option>
           </select>
+          {newCategoryAdder && (
+            <input
+              required
+              className="w-full block text-sm border rounded-lg p-1 mb-6 text-gray-900 border-gray-300 bg-gray-50"
+            />
+          )}
           <label
             className="block text-sm font-medium mb-2 text-white"
             htmlFor="category-input"
@@ -105,31 +141,40 @@ const ProductManagementModal = ({ showModal, onHideModal }) => {
             زیر دسته:
           </label>
           <select
+            required
             ref={userSubcategoryInputRef}
-            className="w-full block text-sm rounded-lg cursor-pointer p-1 mb-6 text-gray-900 border border-gray-300 bg-gray-50"
+            className={`w-full block text-sm rounded-lg cursor-pointer p-1 ${
+              newSubcategoryAdder ? "mb-2" : "mb-6"
+            } text-gray-900 border border-gray-300 bg-gray-50`}
             id="category-input"
+            onChange={userSubcategorySelectedHandler}
           >
-            <option>لطفا یک زیر دسته را انتخاب کنید ...</option>
+            <option value="">لطفا یک زیر دسته را انتخاب کنید ...</option>
             {subCategories.map((category) => (
               <option value={category}>{category}</option>
             ))}
-            <option> زیر دسته مورد نظر خود را وارد کنید.</option>
+            <option value="newSubcategory">
+              زیر دسته مورد نظر خود را وارد کنید.
+            </option>
           </select>
+          {newSubcategoryAdder && (
+            <input
+              required
+              className="w-full block text-sm border rounded-lg p-1 mb-6 text-gray-900 border-gray-300 bg-gray-50"
+            />
+          )}
           <label
             className="block text-sm font-medium mb-2 text-white"
             htmlFor="text_input"
           >
             توضیحات:
           </label>
-          <div className="ltr bg-white">
-            <Weditor
-              // editorState={editorState}
-              toolbarClassName="toolbarClassName"
-              wrapperClassName="wrapperClassName"
-              editorClassName="editorClassName"
-              // onEditorStateChange={this.onEditorStateChange}
-            />
-          </div>
+          <textarea
+            required
+            ref={userDescriptionInputRef}
+            rows="5"
+            className="w-full block text-sm rounded-lg p-2 mb-6 text-gray-900 border border-gray-300 bg-gray-50"
+          ></textarea>
         </div>
         <div className="flex justify-center rounded-t p-4">
           <Button type="submit" className="w-full px-3 bg-slate-800 md:w-1/5">
