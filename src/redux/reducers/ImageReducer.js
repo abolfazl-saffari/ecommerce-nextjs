0b1b5499ea4857317530561b4e8c5ff731301833
Abolfazl Saffari <abolfazl_drx@yahoo.com>
2023-05-25 13:09:50 +0330

    feat: removeImage added to ImageReducer

--- src/redux/reducers/ImageReducer.js
+++ src/redux/reducers/ImageReducer.js
@@ -1,4 +1,4 @@
-import { ADD_IMAGE } from "../types";
+import { ADD_IMAGE, REMOVE_IMAGE } from "../types";
 
 const initialState = {
   Image: "",
@@ -9,6 +9,8 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_IMAGE:
       return { ...state, Image: action.payload.filename };
+    case REMOVE_IMAGE:
+      return { ...state, Image: "" };
     default:
       return state;
   }
